5f5d9fc3796bad51dbd2c29b8e0562f549c59dc1
Tilap <tilap@users.noreply.github.com>
2024-09-19 11:44:34 +0200

    fix: remove scrollview in ScreenContainer
    this is each screen responsability

--- src/components/ScreenContainer.tsx
+++ src/components/ScreenContainer.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import { ScrollView, type ScrollViewProps, StyleSheet, ViewStyle } from "react-native";
+import { SafeAreaView, StyleSheet, ViewStyle } from "react-native";
 import { Box } from "./Box";
-
-const styles = StyleSheet.create({
-  scrollView: {
-    flex: 1,
-  },
-});
 
 const presetStyles = StyleSheet.create({
   page: {
@@ -23,25 +17,20 @@ const presetStyles = StyleSheet.create({
     flex: 1,
     width: "100%",
     height: "100%",
-    paddingBottom: 22,
   },
 });
 
-type Preset = keyof typeof presetStyles;
-
 export function ScreenContainer({
   children,
-  preset = "page",
-  scrollViewProps,
+  preset = "full",
   style,
 }: React.PropsWithChildren<{
-  preset?: Preset;
-  scrollViewProps?: ScrollViewProps;
+  preset?: keyof typeof presetStyles;
   style?: ViewStyle | ViewStyle[];
 }>) {
   return (
-    <ScrollView contentContainerStyle={styles.scrollView} {...scrollViewProps}>
+    <SafeAreaView style={{ flex: 1 }}>
       <Box style={[presetStyles[preset], style]}>{children}</Box>
-    </ScrollView>
+    </SafeAreaView>
   );
 }
